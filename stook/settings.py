21e1dc00937bbed4b3c66ce45cca16dccd987cd8
Bekarys <magauiyanov.b@gmail.com>
2022-05-09 21:32:18 +0600

    beka

--- stook/settings.py
+++ stook/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'stook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  #!!!! if Windows 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
