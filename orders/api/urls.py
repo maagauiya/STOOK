60aa504312938738293a194d9e379ab9f0b1f791
slonoboy <52863393+slonoboy@users.noreply.github.com>
2022-05-10 21:11:02 +0600

    v1

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create-orders/', views.create_orders, name='create_orders'),
-    
+    path('edit-order/<int:pk>', views.edit_order, name='edit_order')
 ]
--- orders/api/views.py
+++ orders/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from cart.models import CartItem
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 @api_view(['POST'])
@@ -12,10 +14,69 @@ from rest_framework import status
 def create_orders(request):
     user = request.user
     data = request.data
-    cart = CartItem.objects.filter(user=user)
     
+    cart = CartItem.objects.filter(user=user)
     if cart.first() == None:
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    country = data.get('country')
+    region = data.get('region')
+    city = data.get('city')
+    address = data.get('address')
+    post_number = data.get('post_number')
+    _status = 'Ordered, waiting supplier confirmation'
+    
+    orders = []
+    
+    for i in cart:
+        product = get_object_or_404(Product, id=i.product.id)
+        supplier = product.user
+        quantity = i.quantity
+        
+        order = Order.objects.create(
+            client = user,
+            supplier = supplier,
+            product = product,
+            quantity = quantity,
+            country = country,
+            region = region,
+            city = city,
+            address = address,
+            post_number = post_number,
+            status = _status
+        )
+        order.save()
+        orders.append(order)
+        
+    _orders = Order.objects.filter(id__in=[i.id for i in orders])
+    serializer = OrderSerializer(_orders, many=True)
+    return Response(serializer.data)
+        
+
+@api_view(['PUT'])
+@permission_classes((IsAuthenticated,))
+def edit_order(request, pk):
+    data = request.data
+    order = get_object_or_404(Order, id=pk)
+    if order.supplier == request.user:
+        if data.get('status'):
+            order.status = data.get('status')
+        
+        if data.get('track_number'):
+            order.track_number = data.get('track_number')
+            
+        if data.get('shipping_date'):
+            order.shipping_date = data.get('shipping_date')    
+        
+        order.save()
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        
+
+    
     
     
     
